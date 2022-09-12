e1c989ceecb562cd54e7ba0eb9d912b8e37567d4
Mehul Tandel <mehultandel.tech@gmail.com>
2022-09-12 20:26:04 +0530

    Added CartIcon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import pizzaImage from "../../assets/pizza.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
     return (
         <React.Fragment>
             <header className={classes.header}>
                 <h1>Hunger Knockouts</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={pizzaImage} alt="A freshly baked pizza!"/>
